19a738acdbf4e164e80ce0c20c96250bf876bd32
Daniel Sousa <daniel.s@crowdbotics.com>
2024-04-17 18:29:18 +0100

    fix: inject cookiecutter extra context directly
    finding relative files isn't reliable across many operating systems and
    package managers combinations, cookiecutter supports injecting the
    context directly instead which works on all platforms/package manager combinations
    
    Changes:
    - pass extra context to the cookiecutter command
    - remove the config file cookiecutter.yaml
    
    Reference:
    https://cookiecutter.readthedocs.io/en/stable/advanced/injecting_context.html
    
    Ticket:
    PLAT-14487

--- index.js
+++ index.js
@@ -52,19 +52,6 @@ import { EVENT } from "./scripts/analytics/constants.js";
 import { askOptIn } from "./scripts/analytics/scripts.js";
 import { sentryMonitoring } from "./scripts/utils/sentry.js";
 
-const pkg = JSON.parse(
-  fs.readFileSync(new URL("package.json", import.meta.url), "utf8")
-);
-
-let sourceDir = path.dirname(path.dirname(process.argv[1]));
-if (fs.existsSync(path.join(sourceDir, pkg.name))) {
-  // npx lib directory
-  sourceDir = path.join(sourceDir, pkg.name);
-} else {
-  // npm lib directory
-  sourceDir = path.join(sourceDir, "lib", "node_modules", pkg.name);
-}
-
 const gitRoot = () => {
   try {
     return path.dirname(findGitRoot(process.cwd()));
@@ -116,10 +103,7 @@ const commands = {
       EnvironmentDependency.Python,
       EnvironmentDependency.PipEnv
     ]);
-    createDemo(
-      path.join(gitRoot(), "demo"),
-      path.join(sourceDir, "cookiecutter.yaml")
-    );
+    createDemo(path.join(gitRoot(), "demo"));
     valid("demo app successfully generated");
   },
   parse: () => {
