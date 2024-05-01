c9ee552285b7860ef0bf2f10155c699a70ba7216
Robert So (robester0403) <85914248+robester0403@users.noreply.github.com>
2024-05-01 10:59:56 -0400

    ASBLY-29 replaced list with constant

--- utils.js
+++ utils.js
@@ -3,6 +3,7 @@ export const generateCommand = (str) => str.join(" ");
 const VALID_MARK = "\u2705";
 const INVALID_MARK = "\u274C";
 const WARNING_MARK = "\u26A0";
+const LOGIN_REQUIRED_COMMANDS = ["help", "logout", "login"];
 
 export const valid = (...args) => {
   console.log(VALID_MARK, ...args);
@@ -25,6 +26,6 @@ export const section = (...args) => {
 export const isUserEnvironment = !process?.env?.CI && !process?.env?.CIRCLE_JOB;
 
 export const isLoginNotReqCommand = (command) => {
-  const allowedCommands = ["help", "logout", "login"];
+  const allowedCommands = LOGIN_REQUIRED_COMMANDS;
   return allowedCommands.includes(command);
 };
